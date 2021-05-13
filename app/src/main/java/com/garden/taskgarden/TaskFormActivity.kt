cde07d85075fb2ced7cfc1831a465831af7d7248
Blake <macdadeblake@gmail.com>
2021-05-13 16:13:53 +1200

    Should now automatically return to main activity when you add a task

--- app/src/main/java/com/garden/taskgarden/TaskFormActivity.kt
+++ app/src/main/java/com/garden/taskgarden/TaskFormActivity.kt
@@ -1,5 +1,6 @@
 package com.garden.taskgarden
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,9 +11,9 @@ import com.garden.taskgarden.R
 import com.garden.taskgarden.Task
 
 class TaskFormActivity : AppCompatActivity() {
-    var taskId: EditText? = null
     var taskName: EditText? = null
     var taskDescription: EditText? = null
+    var taskToCompleteBy: EditText? = null
     private val debugTag = "TaskFormActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +24,16 @@ class TaskFormActivity : AppCompatActivity() {
     fun addTaskForm(view: View?){
         taskName = findViewById(R.id.etForm_Task)
         taskDescription = findViewById(R.id.etForm_Description)
+        taskToCompleteBy = findViewById(R.id.etForm_CompletedBy)
         try {
             val task = Task()
             task.updateTitle(taskName!!.text.toString())
             task.updateDescription(taskDescription!!.text.toString())
+            //task.updateCompletedBy(Integer.parseInt(taskToCompleteBy!!.text.toString()))
+
             DBInterface.addTask(task, this)
+            val intent = Intent(this@TaskFormActivity, MainActivity::class.java)
+            startActivity(intent)
         } catch (e: Exception) {
             Log.d(debugTag, "Got $e while trying to add task in TaskFormActivity!")
         }
